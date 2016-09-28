0dec27a812661f7949e345a6671bff40652b3919
pasaunders <kaorti@gmail.com>
2016-09-28 16:14:06 -0700

    refactor client requests in order to ahdn control to our proxy server

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -4,28 +4,16 @@
   repos.requestRepos = function(callback) {
     // TODO: refactor these requests into an $.ajax call
     $.when(
-     $.ajax({
-       url: 'https://api.github.com/users/codefellows-seattle-301d12/repos' +
-            '?per_page=10' +
-            '&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data) {
-         // NOTE: since the 'data' paramter comes back as an
-         // array of objects, we can reassign allRepos below.
-         repos.allRepos = data;
-       }
+     $.get('/github/users/codefellows-seattle-301d12')
+     .done(function(data) {
+       repos.allRepos = data;
      }),
-     $.ajax({
-       url: 'https://api.github.com/users/patci/followers' +
+     $.get('github/users/patci/followers' +
             '?per_page=5' +
-            '&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data) {
-         repos.followers = data;
-       }
-     })
+            '&sort=updated')
+            .done(function(data) {
+              repos.followers = data;
+            })
     ).done(callback);
   };
 
